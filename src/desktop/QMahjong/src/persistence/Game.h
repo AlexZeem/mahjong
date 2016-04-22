9ff949dd546e56d521ddf6d7e4a3bfd3fbe46c30
ArRana <xholycockx@gmail.com>
2016-04-22 23:28:56 +0300

    Add comments to code

--- src/desktop/QMahjong/src/persistence/Game.h
+++ src/desktop/QMahjong/src/persistence/Game.h
@@ -1,26 +1,32 @@
-#pragma once
+#pragma once // предпроцессорная дериктива, контроль подключения исходника только 1 раз при компиляции
 #include <vector>
 #include <string>
 #include <iostream>
 
+// некоторео множество, создание для группировки неких идентификаторов.
 namespace persistence {
 
 class Game
 {
 public:
-    Game()
+    Game() // конструктор по умолчанию
     { }
 
     Game(const unsigned long& _id) :
+    /* конструктор который инициирует объект параметром _id
+    (константа ссылки(переданный параметр не будет изменен)).
+        // инициализируемый объект*/
         gameId(_id)
     { }
 
-    Game(const std::string& _d, const std::string& _w) :
+    Game(const std::string& _d, const std::string& _w) : // конструктор который инициирует объекты параметрами  _d, _w
+        // инициализируемые объекты
         data(_d),
         winner (_w)
     { }
 
-    Game(const std::vector<int>& _s) :
+    Game(const std::vector<int>& _s) : // конструктор который инициирует объект параметром _s
+        // инициализируемый объект
         score(_s)
     { }
 
